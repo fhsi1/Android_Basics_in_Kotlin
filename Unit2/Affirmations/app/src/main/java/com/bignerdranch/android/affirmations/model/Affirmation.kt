9d47b4578307ae5fbf6a9630f097d0b7aa97cdf1
fhsi1 <eugenie8@kakao.com>
2022-01-05 19:40:01 +0900

    [UPEATE] Unit2

---
+++ Unit2/Affirmations/app/src/main/java/com/bignerdranch/android/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
